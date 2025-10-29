8e8800e29adcc377b306f4b07931576233920ef4
Arwen Ferro <aferro12@terpmail.umd.edu>
2025-10-29 19:27:38 -0400

    it's broken

---
+++ aca/src/input/ouput_idea.rs
@@ -0,0 +1,32 @@
+
+//atomic start
+fn main() {
+    let mut z = 0;
+    let mut x = 12;
+    x += 1;
+
+    let y = true;
+
+    test(z);
+//crash point
+
+//re-exec
+    let mut z = 0;
+    let mut x = 12;
+    x += 1;
+
+    let y = true;
+
+    test(z);
+//end new stuff 
+
+    if y {
+        x+= 2;
+    }
+}
+
+//atomic start
+//nids(z)
+fn test(z:& mut u16) -> () {
+    z += 20;
+}
--- aca/src/main.rs
+++ aca/src/main.rs
@@ -1,3 +1,65 @@
+use regex::Regex;
+use std::fs::{read_to_string, File, OpenOptions};
+use std::io::{self, BufReader, BufWriter, Write, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    
+}
+
+
+
+fn copy_sections(file: &str) {
+    let re_crash = Regex::new(r"^//crash point").unwrap();
+    let re_atomic = Regex::new(r"//atomic start").unwrap();
+    let re_nids = Regex::new(r"nids").unwrap();
+    let re_nid_vars = Regex::new(r"([a-z\_]+)").unwrap();
+    let let_binding = Regex::new(r"").unwrap();
+
+
+    // at a crash point, copy everything again so it "reexecutes". keep 
+    // track of nids and everything else reset.
+
+    let mut r = Vec::new();
+    let mut lines_since_last_atomic = vec![];
+    let out_file_path = "output.rs";
+
+    let output_file = OpenOptions::new()
+    .write(true)
+    .create(true)
+    .truncate(true)
+    .open(out_file_path).unwrap();
+
+    for line in read_to_string(file).unwrap().lines() {
+        if re_atomic.is_match(line) {
+            lines_since_last_atomic = vec![];
+        } else {
+            lines_since_last_atomic.push(line);
+        }
+
+        if re_nids.is_match(line) {
+            let mut nids = vec![];
+            for (_, [var]) in re_nid_vars.captures_iter(line).map(|c| c.extract()) {
+                nids.push(var);
+            }
+        }
+
+        if {
+
+        }
+
+        if re_crash.is_match(line) {
+            let mut writer = BufWriter::new(output_file);
+
+            for line_result in lines_since_last_atomic {
+                let line = line_result; 
+                writeln!(writer, "{}", line); 
+            }
+
+            writer.flush();
+            lines_since_last_atomic = vec![];
+
+        }
+
+    }
+
 }
